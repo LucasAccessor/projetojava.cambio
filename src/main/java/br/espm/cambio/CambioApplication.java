6d12fb55acf2d40aa790ec0eeb00acc74b5828d1
Lucas Eiji <lucasaccessor@hotmail.com>
2022-05-12 12:24:50 -0300

    Get & Post iniciais

--- src/main/java/br/espm/cambio/CambioApplication.java
+++ src/main/java/br/espm/cambio/CambioApplication.java
@@ -3,7 +3,7 @@ package br.espm.cambio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //notação é algo que informa o navegador
 public class CambioApplication {
 
 	public static void main(String[] args) {
